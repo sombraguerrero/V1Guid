141402c58c179eb785d399a40480b7f0e35c980c
Robert Setter <r.setter@comcast.net>
2024-04-20 05:28:39 -0500

    SQLExpress exercise

--- V1Guid/Program.cs
+++ V1Guid/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.InteropServices;
 using System.Text;
+using System.Data.SqlClient;
 
 [DllImport("rpcrt4.dll", SetLastError = true)] //DLL still present as of Windows 11 - both architectures
 static extern int UuidCreateSequential(out Guid guid); //Function names are case-sensitive per the output of dumpbin /exports 
@@ -9,14 +10,45 @@ int num;
 Guid g;
 if (args.Length == 1 && int.TryParse(args[0], out num))
 {
-    for (int i = 0; i < num; i++)
+    //A BIN2 coallation on the database would seem to make this more optimal for case-sensitivity and the use of the binary
+    //Guid as a primary key
+    using (SqlConnection conn = new SqlConnection(Environment.GetEnvironmentVariable("tsql_test", EnvironmentVariableTarget.User)))
     {
-        if (UuidCreateSequential(out g) == RPC_S_OK)
+        try
         {
-            Console.WriteLine(g.ToString().ToUpper());
+            conn.Open();
+            using (SqlTransaction trans = conn.BeginTransaction())
+            {
+                SqlCommand command = new SqlCommand("INSERT INTO [dbo].[myguids] (ordered) values (@someGuid)", conn, trans);
+                command.Parameters.Add("someguid", System.Data.SqlDbType.VarBinary);
+                for (int i = 0; i < num; i++)
+                {
+                    try
+                    {
+                        if (UuidCreateSequential(out g) == RPC_S_OK)
+                        {
+                            string gString = g.ToString().ToUpper();
+                            string gSubString = gString.Substring(14, 4) + gString.Substring(9, 4) + gString.Substring(0, 8) + gString.Substring(19, 4) + gString.Substring(24);
+                            Console.WriteLine(gSubString);
+                            command.Parameters["someguid"].Value = Encoding.Default.GetBytes(gSubString);
+                            command.ExecuteNonQuery();
+                        }
+                        else
+                            Console.WriteLine("Creation of this GUID failed!");
+                    }
+                    catch (SqlException ex)
+                    {
+                        Console.WriteLine("SqlException inside the loop: {0}", ex.Message);
+                        trans.Rollback();
+                    }
+                }
+                trans.Commit();
+            }
         }
-        else
-            Console.WriteLine("Creation of this GUID failed!");
+        catch (SqlException ex)
+        {
+            Console.Error.WriteLine("SqlException outside the loop: {0}", ex.Message);
+        }
     }
 }
 else if (args.Length == 1 && args[0].ToLower().Equals("--stdin"))
@@ -24,14 +56,45 @@ else if (args.Length == 1 && args[0].ToLower().Equals("--stdin"))
     Console.Write("Number of Guids: ");
     if (int.TryParse(Console.ReadLine(), out num))
     {
-        for (int i = 0; i < num; i++)
+        //A BIN2 coallation on the database would seem to make this more optimal for case-sensitivity and the use of the binary
+        //Guid as a primary key
+        using (SqlConnection conn = new SqlConnection(Environment.GetEnvironmentVariable("tsql_test", EnvironmentVariableTarget.User)))
         {
-            if (UuidCreateSequential(out g) == RPC_S_OK)
+            try
             {
-                Console.WriteLine(g.ToString().ToUpper());
+                conn.Open();
+                using (SqlTransaction trans = conn.BeginTransaction())
+                {
+                    SqlCommand command = new SqlCommand("INSERT INTO [dbo].[myguids] (ordered) values (@someGuid)", conn, trans);
+                    command.Parameters.Add("someguid", System.Data.SqlDbType.VarBinary);
+                    for (int i = 0; i < num; i++)
+                    {
+                        try
+                        {
+                            if (UuidCreateSequential(out g) == RPC_S_OK)
+                            {
+                                string gString = g.ToString().ToUpper();
+                                string gSubString = gString.Substring(14, 4) + gString.Substring(9, 4) + gString.Substring(0, 8) + gString.Substring(19, 4) + gString.Substring(24);
+                                Console.WriteLine(gSubString);
+                                command.Parameters["someguid"].Value = Encoding.Default.GetBytes(gSubString);
+                                command.ExecuteNonQuery();
+                            }
+                            else
+                                Console.WriteLine("Creation of this GUID failed!");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.ToString());
+                            trans.Rollback();
+                        }
+                    }
+                    trans.Commit();
+                }
             }
-            else
-                Console.WriteLine("Creation of this GUID failed!");
+            catch (SqlException ex)
+            {
+                Console.Error.WriteLine(ex.ToString());
+            }
         }
     }
 }
@@ -41,7 +104,25 @@ else if (UuidCreateSequential(out g) == RPC_S_OK)
     string gString = g.ToString().ToUpper();
     string gSubString = gString.Substring(14, 4) + gString.Substring(9, 4) + gString.Substring(0, 8) + gString.Substring(19, 4) + gString.Substring(24);
     byte[] gSubStringBytes = Encoding.Default.GetBytes(gSubString);
-    Console.WriteLine("Original: {0}\r\nSubstringed: {1}\r\nHex: {2}\r\nVia BitConverter: {3}", gString, gSubString, Convert.ToHexString(gSubStringBytes), BitConverter.ToString(gSubStringBytes).Replace("-", string.Empty));
+    string hexString = @"0x" + Convert.ToHexString(gSubStringBytes);
+    Console.WriteLine("Original: {0}\r\nSubstringed: {1}\r\nHex: {2}\r\nVia BitConverter: {3}\r\nByte array length: {4}", gString, gSubString, hexString, @"0x" + BitConverter.ToString(gSubStringBytes).Replace("-", string.Empty), gSubStringBytes.Length);
+
+    //A BIN2 coallation on the database would seem to make this more optimal for case-sensitivity and the use of the binary
+    //Guid as a primary key
+    using (SqlConnection conn = new SqlConnection(Environment.GetEnvironmentVariable("tsql_test", EnvironmentVariableTarget.User)))
+    {
+        try
+        {
+            conn.Open();
+            SqlCommand command = new SqlCommand("INSERT INTO [dbo].[myguids] (ordered) values (@someGuid)", conn);
+            command.Parameters.AddWithValue("someguid", gSubStringBytes);
+            command.ExecuteNonQuery();
+        }
+        catch (SqlException ex)
+        {
+            Console.Error.WriteLine(ex.ToString());
+        }
+    }
 }
 else
 {
